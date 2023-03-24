11c61bf9ee6abb7f8022836c223ff706ca9c74fe
idonun <mc.natsu831@gmail.com>
2023-03-24 23:43:45 +0900

    Add DI

---
+++ Blueprint/DI.swift
@@ -0,0 +1,18 @@
+//
+//  Container.swift
+//  Blueprint
+//
+//  Created by Natsuki Idota on 2023/03/24.
+//
+
+import Swinject
+
+let container: Container = {
+    let container = Container()
+    Assembler(container: container).apply(assemblies: [
+        ApiAssembly()
+    ])
+    return container
+}()
+
+var resolver: (() -> Resolver)!
