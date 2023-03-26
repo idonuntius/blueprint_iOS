ed63e22b7534a59c8de61a7368d4e7772f59a0d9
idonun <mc.natsu831@gmail.com>
2023-03-26 12:25:18 +0900

    Fix path in GithubApi

--- Blueprint/Data/Api/GithubApi.swift
+++ Blueprint/Data/Api/GithubApi.swift
@@ -11,13 +11,9 @@ import Alamofire
 public struct GithubApi {
     private let baseApiUrl = URL(string: "https://api.github.com")!
     
-    public func getRepos() async throws -> [RepoEntity] {
-        var urlComponents = URLComponents(url: baseApiUrl.appendingPathComponent("/search/repositories"), resolvingAgainstBaseURL: true)!
-        urlComponents.queryItems = [
-            URLQueryItem(name: "q", value: "swift"),
-            URLQueryItem(name: "sort", value: "stars"),
-        ]
-        return try await AF.request(urlComponents)
-            .publish([RepoEntity].self)
+    public func fetchRepos() async throws -> [RepoEntity] {
+        let urlComponents = URLComponents(url: baseApiUrl.appendingPathComponent("/orgs/apple/repos"),
+                                          resolvingAgainstBaseURL: true)!
+        return try await AF.request(urlComponents).publish([RepoEntity].self)
     }
 }
