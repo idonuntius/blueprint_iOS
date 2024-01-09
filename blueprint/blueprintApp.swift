e730970e294c42a20949b9def7c6b4543aadaa8c
idonun <mc.natsu831@gmail.com>
2024-01-09 22:32:02 +0900

    Initial Commit

---
+++ blueprint/blueprintApp.swift
@@ -0,0 +1,17 @@
+//
+//  blueprintApp.swift
+//  blueprint
+//
+//  Created by Natsuki Idota on 2024/01/09.
+//
+
+import SwiftUI
+
+@main
+struct blueprintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
